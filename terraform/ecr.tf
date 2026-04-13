701af85ed5d880eff086f0f569c1c699b8687a89
Alexis Webber <alexis.webber@maritz.com>
2026-04-12 23:22:41 -0500

    Add ECR resources for Node.js GitOps deployment

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,4 @@
-# ECR Repository
+# ECR Repository for Python Flask App
 resource "aws_ecr_repository" "app" {
   name                 = "app-repository"
   image_tag_mutability = "MUTABLE"
@@ -8,23 +8,23 @@ resource "aws_ecr_repository" "app" {
   }
 
   tags = {
-    Name = "tc2-redo-app"
+    Name = "hello-app"
   }
 }
 
-# ECR Repository Policy
+# ECR Repository Policy for Flask App
 resource "aws_ecr_repository_policy" "app" {
   repository = aws_ecr_repository.app.name
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Sid    = "AllowPullFromEKS"
-        Effect = "Allow"
+        Sid    = "AllowPullFromEKS",
+        Effect = "Allow",
         Principal = {
           AWS = aws_iam_role.eks_node_group.arn
-        }
+        },
         Action = [
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage",
@@ -33,4 +33,41 @@ resource "aws_ecr_repository_policy" "app" {
       }
     ]
   })
-} 
+}
+
+# ECR Repository for Node.js App
+resource "aws_ecr_repository" "node_app" {
+  name                 = "node-app-repository"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "node-app"
+  }
+}
+
+# ECR Repository Policy for Node.js App
+resource "aws_ecr_repository_policy" "node_app" {
+  repository = aws_ecr_repository.node_app.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowPullFromEKS",
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_iam_role.eks_node_group.arn
+        },
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+      }
+    ]
+  })
+}
